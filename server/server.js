d8afdac737085d6fbbf6e5b714cfc3fc814bad2b
NehaBinu <nehabinu17@gmail.com>
2026-09-23 00:37:29 +0530

    Improve MongoDB connection logging

--- server/server.js
+++ server/server.js
@@ -22,7 +22,17 @@ app.use('/api/apply', applyRoutes);
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch((err) => console.error('MongoDB connection error:', err));
+mongoose.connection.on('connected', () => {
+  console.log('✅ Mongoose connected');
+});
 
+mongoose.connection.on('error', (err) => {
+  console.error('❌ Mongoose error:', err);
+});
+
+mongoose.connection.on('disconnected', () => {
+  console.log('❌ Mongoose disconnected');
+});
 app.get('/', (req, res) => {
   res.send('Sportzzbook API is running');
 });
